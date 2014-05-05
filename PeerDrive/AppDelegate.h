8781ea768cc448dcd5bf8492bacf193e07e377c6
Hnin Htet <hninhtet@rs-v.com>
2014-05-05 10:38:52 +0800

    Initial Commit

---
+++ PeerDrive/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PeerDrive
+//
+//  Created by Rayser on 5/5/14.
+//  Copyright (c) 2014 Herenow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
